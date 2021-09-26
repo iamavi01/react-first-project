cab76bb3a657c517d22fbd638a09bd6145dde808
iamavi01 <bhagwatithakur011@gmail.com>
2021-09-26 12:29:56 +0545

    git initial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./app.css"
+import "/home/abhithakur/portfolio/node_modules/bootstrap/dist/css/bootstrap.min.css"
+
+
+
+function App(){
+  return(
+    <h1>hellow world</h1>
+  )
+}
+export default App
